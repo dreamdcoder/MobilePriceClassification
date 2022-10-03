d5e4fff3debf666195d487e4eadd4bb3563e8e19
yogeshjawalkar <yogi100888@gmail.com>
2022-10-03 13:53:34 +0530

    Commit-002

---
+++ main.py
@@ -0,0 +1,11 @@
+import pandas as pd
+data = pd.read_csv("Dataset\\train.csv")
+
+from sdv.tabular import GaussianCopula
+model = GaussianCopula()
+model.fit(data)
+sample = model.sample(1000)
+sample.to_csv("Dataset\\test2.csv")
+print(sample.head())
+
+
